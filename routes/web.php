92a3d49bacebe75d362e833a04f08fa583293932
RobbertKlockaerts <robbert.klockaerts@gmail.comm>
2021-06-15 16:56:04 +0200

    static-pages-routes and Header+Footer blade

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,8 @@
 
 use Illuminate\Support\Facades\Route;
 
+use App\Http\Controllers\PagesController;
+
 /*
 |--------------------------------------------------------------------------
 | Web Routes
@@ -13,6 +15,9 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+
+Route::get('/', [PagesController::class, 'index']); 
+Route::get('/about', [PagesController::class, 'about']); 
+Route::get('/contact', [PagesController::class, 'contact']); 
+
+
