351f9055f63d6a90c58c8f04ce005e2d6f3c38e0
Marcelo Romero-Cors <luisromeroaraya@gmail.com>
2021-06-16 11:27:25 +0200

    Modify UserFactory , User Migration and add User Seeder

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -21,10 +21,14 @@ class UserFactory extends Factory
      * @return array
      */
     public function definition()
-    {
+    {   
+        $name = $this->faker->name();
+        $replace = array(' ', '.');
+        $username = substr(str_replace($replace,'',strtolower($name)),0).rand(1,999);
         return [
-            'name' => $this->faker->name(),
-            'email' => $this->faker->unique()->safeEmail(),
+            'name' => $name,
+            'username' => $username,
+            'email' => $username.'@mail.com',
             'email_verified_at' => now(),
             'password' => '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi', // password
             'remember_token' => Str::random(10),
