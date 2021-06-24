d33ae0079931416c154c5d30ec3f8f411b9b08e9
RobbertKlockaerts <robbert.klockaerts@gmail.comm>
2021-06-24 13:25:03 +0200

    functional slider not dynamic yet

--- resources/views/gallery/index.blade.php
+++ resources/views/gallery/index.blade.php
@@ -1,20 +1,7 @@
 <x-guest-layout>
-    {{-- <div class="container mx-auto px-4"> 
-    <section class="pt-8 px-4">
-        <div class="flex flex-wrap -mx-4">
-          <div class="md:w-1/3 px-4 mb-8"><img class="rounded shadow-md" src="/images/Feriadart0.png" alt=""></div>
-          <div class="md:w-1/3 px-4 mb-8"><img class="rounded shadow-md" src="/images/Feriadart0.png" alt=""></div>
-          <div class="md:w-1/3 px-4 mb-8"><img class="rounded shadow-md" src="/images/Feriadart0.png" alt=""></div>
-          <div class="md:w-1/3 px-4 mb-8"><img class="rounded shadow-md" src="/images/Feriadart0.png" alt=""></div>
-          <div class="md:w-1/3 px-4 mb-8"><img class="rounded shadow-md" src="/images/Feriadart0.png" alt=""></div>
-          <div class="md:w-1/3 px-4 mb-8"><img class="rounded shadow-md" src="/images/Feriadart0.png" alt=""></div>
-        </div>
-      </section>
-    </div> --}}
-
+    
     <head>
       <style>
-      /*each image is referenced twice in the HTML, but each image only needs to be updated in the CSS*/
       .image1 {
         content: url("/images/Feriadart0.png")
       }
@@ -38,62 +25,40 @@
     </head> 
     
     
-      <section class="mx-auto max-w-2xl">
-        <h2 class="text-4xl text-center tracking-wide font-extrabold font-serif leading-loose mb-2">Gallery</h2>
-        <div class="shadow-2xl relative">
+      <section class="mx-auto mt-10">
+        <h2 class="text-4xl text-center font-bold mb-10">Gallery</h2>
+
+        <div class="shadow-2xl mx-auto max-w-3xl">
           <!-- large image on slides -->
           <div class="mySlides hidden">
-            <div class="image1 w-full object-cover"></div>
+            <div class="image1 w-full object-cover rounded shadow-md"></div>
           </div>
           <div class="mySlides hidden">
-            <div class="image2 w-full object-cover"></div>
+            <div class="image2 w-full object-cover rounded shadow-md"></div>
           </div>
           <div class="mySlides hidden">
-            <div class="image3 w-full object-cover"></div>
+            <div class="image3 w-full object-cover rounded shadow-md"></div>
           </div>
           <div class="mySlides hidden">
-            <div class="image4 w-full object-cover"></div>
+            <div class="image4 w-full object-cover rounded shadow-md"></div>
           </div>
           <div class="mySlides hidden">
-            <div class="image5 w-full object-cover"></div>
+            <div class="image5 w-full object-cover rounded shadow-md"></div>
           </div>
     
-          <!-- butttons -->
-       
-    
-          <!-- image description -->
-          {{-- <div class="text-center text-white font-light tracking-wider bg-gray-800 py-2">
-            <p id="caption"></p>
-          </div> --}}
-{{--  
-
-          <a class="absolute left-0 inset-y-0 flex items-center -mt-32 px-4 text-white hover:text-gray-800 cursor-pointer text-3xl font-extrabold" onclick="plusSlides(-1)">❮</a>
-          <a class="absolute right-0 inset-y-0 flex items-center -mt-32 px-4 text-white hover:text-gray-800 cursor-pointer text-3xl font-extrabold" onclick="plusSlides(1)">❯</a> --}}
-{{--      --}}
-          <!-- smaller images under description -->
-          <div class="flex">
-            <div>
-              <img class="image1 description h-24 opacity-50 hover:opacity-100 cursor-pointer" src="#" onclick="currentSlide(1)">
-            </div>
-            <div>
-              <img class="image2 description h-24 opacity-50 hover:opacity-100 cursor-pointer" src="#" onclick="currentSlide(2)" >
-            </div>
-            <div>
-              <img class="image3 description h-24 opacity-50 hover:opacity-100 cursor-pointer" src="#" onclick="currentSlide(3)" >
-            </div>
-            <div>
-              <img class="image4 description h-24 opacity-50 hover:opacity-100 cursor-pointer" src="#" onclick="currentSlide(4)">
-            </div>
-            <div>
-              <img class="image5 description h-24 opacity-50 hover:opacity-100 cursor-pointer" src="#" onclick="currentSlide(5)">
-            </div>
-          </div>
+         
+          <div class="flex bg-gray-400 rounded shadow-md mx-auto p-4">
+            
+              <a class="mx-auto text-black hover:text-gray-800 cursor-pointer text-3xl underline" onclick="plusSlides(-1)">Previous</a>
+              <a class="mx-auto text-black hover:text-gray-800 cursor-pointer text-3xl underline" onclick="plusSlides(1)">Next</a>
+            
+             </div>
         </div> 
         
-      </section>
+   
 
       <div class="container mx-auto px-4"> 
-        <section class="pt-8 px-4">
+        <section class="pt-8 w-auto px-4">
             <div class="flex flex-wrap -mx-4">
               <div class="md:w-1/3 px-4 mb-8"><img class="rounded shadow-md cursor-pointer" src="/images/Feriadart0.png" onclick="currentSlide(1)" alt=""></div>
               <div class="md:w-1/3 px-4 mb-8"><img class="rounded shadow-md cursor-pointer" src="/images/Feriadart1.png" onclick="currentSlide(2)" alt=""></div>
@@ -105,9 +70,9 @@
           </section>
         </div>
     
-    
+      </section>
       <script>
-        //JS to switch slides and replace text in bar//
+      
         var slideIndex = 1;
         showSlides(slideIndex);
     
@@ -123,7 +88,7 @@
           var i;
           var slides = document.getElementsByClassName("mySlides");
           var dots = document.getElementsByClassName("description");
-          var captionText = document.getElementById("caption");
+          // var captionText = document.getElementById("caption");
           if (n > slides.length) {
             slideIndex = 1
           }
