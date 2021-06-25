4f8e601463b52916761064aeb7b091610ffc2baa
Marcelo Romero-Cors <luisromeroaraya@gmail.com>
2021-06-25 09:50:11 +0200

    Add Dynamic Dashboard Event Table

--- app/Http/Controllers/BackofficeEventsController.php
+++ app/Http/Controllers/BackofficeEventsController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Event;
 use Illuminate\Http\Request;
 
 class BackofficeEventsController extends Controller
@@ -13,7 +14,10 @@ class BackofficeEventsController extends Controller
      */
     public function index()
     {
-        return view('backoffice.events');
+        $events = Event::all();
+        return view('backoffice.events', [
+            'events' => $events
+        ]);
 
     }
 
--- resources/views/backoffice/events.blade.php
+++ resources/views/backoffice/events.blade.php
@@ -4,13 +4,79 @@
             {{ __('Events') }}
         </h2>
     </x-slot>
-    <div class="py-12">
-        <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
-            <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
-                <div class="p-6 bg-white border-b border-gray-200">
-                    {{ __('Events') }}
+    <!-- This example requires Tailwind CSS v2.0+ -->
+    <div class="flex flex-col py-14">
+        <div class="-my-2 overflow-x-auto sm:-mx-6 lg:-mx-8">
+            <div class="py-2 align-middle inline-block min-w-full sm:px-6 lg:px-8">
+                <div class="shadow overflow-hidden border-b border-gray-200 sm:rounded-lg">
+                    <table class="w-11/12 divide-y divide-gray-200 mx-auto">
+                        <thead class="bg-gray-50">
+                            <tr>
+                                <th scope="col" class="px-4 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">
+                                    Date
+                                </th>
+                                <th scope="col" class="px-4 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">
+                                    Name
+                                </th>
+                                <th scope="col" class="px-4 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">
+                                    Place
+                                </th>
+                                <th scope="col" class="px-4 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">
+                                    Contact
+                                </th>
+                                <th scope="col" class="px-4 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">
+                                    Url
+                                </th>
+                                <th scope="col" class="relative px-4 py-3">
+                                    <span class="sr-only">Edit</span>
+                                </th>
+                            </tr>
+                        </thead>
+                        <tbody class="bg-white divide-y divide-gray-200">
+                            @foreach ($events as $event)
+                            <tr>
+                                <td class="px-4 py-4 whitespace-normal">
+                                    <div class="flex items-center">
+                                        <div class="flex-shrink-0 h-10 w-10">
+                                            <img class="h-10 w-10 rounded-full" src="{{ $event->img_src }}" alt="{{ $event->name }}">
+                                        </div>
+                                        <div class="ml-4">
+                                            <div class="text-sm font-medium text-gray-900">
+                                                {{ $event->date_start }}
+                                            </div>
+                                            <div class="text-sm font-medium text-gray-500">
+                                                {{ $event->date_end }}
+                                            </div>
+                                        </div>
+                                    </div>
+                                </td>
+                                <td class="px-4 py-4 whitespace-normal">
+                                    <span class="px-2 inline-flex text-xs leading-5 font-semibold rounded-full bg-green-100 text-green-800">
+                                        {{ $event->name }}
+                                    </span>
+                                </td>
+                                <td class="px-4 py-4 whitespace-normal">
+                                    <div class="text-sm text-gray-900">{{ $event->place }}</div>
+                                    <div class="text-sm text-gray-500">{{ $event->address }}</div>
+                                </td>
+                                <td class="px-4 py-4 whitespace-normal text-sm text-gray-500">
+                                    <div class="text-sm text-gray-900">{{ $event->telephone }}</div>
+                                    <div class="text-sm text-gray-500">{{ $event->email }}</div>
+                                </td>
+                                <td class="px-4 py-4 whitespace-normal text-sm text-gray-500">
+                                    <div class="text-sm text-gray-900">{{ $event->url }}</div>
+                                    <div class="text-sm text-gray-500">{{ $event->img_src }}</div>
+                                </td>
+                                <td class="px-4 py-4 whitespace-normal text-right text-sm font-medium">
+                                    <a href="#" class="text-indigo-600 hover:text-indigo-900">Edit</a>
+                                </td>
+                            </tr>
+                            @endforeach
+                        </tbody>
+                    </table>
                 </div>
             </div>
         </div>
     </div>
+
 </x-app-layout>
