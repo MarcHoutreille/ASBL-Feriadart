0fc677b76f8a12960a639e3a78dd7fa87e00d882
Marcelo Romero-Cors <luisromeroaraya@gmail.com>
2021-06-28 10:39:10 +0200

    Added About Us text

--- resources/views/about.blade.php
+++ resources/views/about.blade.php
@@ -3,7 +3,9 @@
         <div class="container px-5 py-24 mx-auto">
             <div class="flex flex-col text-center w-full mb-20">
                 <h1 class="text-2xl font-medium title-font mb-4 text-gray-900 tracking-widest">{{ __('About Us') }}</h1>
-                <p class="lg:w-2/3 mx-auto leading-relaxed text-base">Whatever cardigan tote bag tumblr hexagon brooklyn asymmetrical gentrify, subway tile poke farm-to-table. Franzen you probably haven't heard of them.</p>
+                <p class="lg:w-full mx-auto leading-relaxed text-justify text-base mt-14">L'asbl Feria d’Art est un marché qui souhaite rendre l’art accessible au plus grand nombre et ce en désacralisant l’achat d’une oeuvre d’art tout en offrant une expérience inclusive pour les artistes et le public. </p>
+                <p class="lg:w-full mx-auto leading-relaxed text-justify text-base mt-14">L’une des cinq organisatrices, de retour de quelques semaines passées au coeur du milieu artistique chilien, nous a parlé de petits marchés d’art organisés dans différentes villes du pays. Elle nous a raconté l’engagement (politique, social, etc.) de ces artistes, de leur communauté et des techniques qu’ils emploient afin de rendre leur art accessible à tout public tout en se rémunérant dignement par la vente de celui-ci. </p>
+                <p class="lg:w-full mx-auto leading-relaxed text-justify text-base mt-14">C’est lors d’une soirée passée ensemble à Bruxelles en temps de Covid que l’envie presque évidente nous est venue de tenter le coup ici. Certaines d’entre nous sont directement concernées par la difficulté de vendre de l’art lorsque celui-ci ne correspond pas forcément à l’idée que l’on se fait de l’Artiste et nous le sommes à l’unanimité lorsqu’il s’agit de se confronter aux prix d’achat de l’Art. C’est pourquoi, loin de vouloir dévaloriser le travail de l’artiste, nous ouvrons une nouvelle porte pour le dévoiler.</p>
             </div>
             <div class="flex flex-wrap -m-4">
                 <div class="p-4 lg:w-1/2">
@@ -66,6 +68,33 @@
                         <div class="flex-grow sm:pl-8">
                             <h2 class="title-font font-medium text-lg text-gray-900">Atticus Finch</h2>
                             <h3 class="text-gray-500 mb-3">UI Developer</h3>
+                            <p class="mb-4">DIY tote bag drinking vinegar cronut adaptogen squid fanny pack vaporware.</p>
+                            <span class="inline-flex">
+                                <a class="text-gray-500">
+                                    <svg fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" class="w-5 h-5" viewBox="0 0 24 24">
+                                        <path d="M18 2h-3a5 5 0 00-5 5v3H7v4h3v8h4v-8h3l1-4h-4V7a1 1 0 011-1h3z"></path>
+                                    </svg>
+                                </a>
+                                <a class="ml-2 text-gray-500">
+                                    <svg fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" class="w-5 h-5" viewBox="0 0 24 24">
+                                        <path d="M23 3a10.9 10.9 0 01-3.14 1.53 4.48 4.48 0 00-7.86 3v1A10.66 10.66 0 013 4s-4 9 5 13a11.64 11.64 0 01-7 2c9 5 20 0 20-11.5a4.5 4.5 0 00-.08-.83A7.72 7.72 0 0023 3z"></path>
+                                    </svg>
+                                </a>
+                                <a class="ml-2 text-gray-500">
+                                    <svg fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" class="w-5 h-5" viewBox="0 0 24 24">
+                                        <path d="M21 11.5a8.38 8.38 0 01-.9 3.8 8.5 8.5 0 01-7.6 4.7 8.38 8.38 0 01-3.8-.9L3 21l1.9-5.7a8.38 8.38 0 01-.9-3.8 8.5 8.5 0 014.7-7.6 8.38 8.38 0 013.8-.9h.5a8.48 8.48 0 018 8v.5z"></path>
+                                    </svg>
+                                </a>
+                            </span>
+                        </div>
+                    </div>
+                </div>
+                <div class="p-4 lg:w-1/2">
+                    <div class="h-full flex sm:flex-row flex-col items-center sm:justify-start justify-center text-center sm:text-left">
+                        <img alt="team" class="flex-shrink-0 rounded-lg w-48 h-48 object-cover object-center sm:mb-0 mb-4" src="https://dummyimage.com/206x206">
+                        <div class="flex-grow sm:pl-8">
+                            <h2 class="title-font font-medium text-lg text-gray-900">Henry Letham</h2>
+                            <h3 class="text-gray-500 mb-3">Designer</h3>
                             <p class="mb-4">DIY tote bag drinking vinegar cronut adaptogen squid fanny pack vaporware.</p>
                             <span class="inline-flex">
                                 <a class="text-gray-500">
