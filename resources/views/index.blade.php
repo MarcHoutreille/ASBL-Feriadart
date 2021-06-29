cff1307306c71d0d858a578afda12421e96c7569
Marcelo Romero-Cors <luisromeroaraya@gmail.com>
2021-06-29 15:17:53 +0200

    Add Home Static Header

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -2,13 +2,13 @@
     <section class="text-gray-600 body-font overflow-hidden">
         <div class="container mx-auto flex px-5 py-24 md:flex-row flex-col items-center">
             <div class="lg:max-w-lg lg:w-full md:w-1/2 w-5/6 mb-10 md:mb-0">
-                <img class="object-cover object-center rounded" alt="hero" src="https://dummyimage.com/720x600">
+                <img class="object-cover object-center rounded" alt="Inscription" src="https://scontent.fcrl1-1.fna.fbcdn.net/v/t1.6435-9/117037004_658765718067234_5865890609677179388_n.jpg?_nc_cat=103&ccb=1-3&_nc_sid=973b4a&_nc_ohc=Lm1_fQfgys0AX-8ga7k&_nc_ht=scontent.fcrl1-1.fna&oh=bab8aa631390e2787998de4b316e852e&oe=60E004D4">
             </div>
             <div class="lg:flex-grow md:w-1/2 lg:pl-24 md:pl-16 flex flex-col md:items-start md:text-left items-center text-center">
-                <h1 class="title-font sm:text-4xl text-3xl mb-4 font-medium text-gray-900">Before they sold out
-                    <br class="hidden lg:inline-block">readymade gluten
+                <h1 class="title-font sm:text-4xl text-3xl mb-4 font-medium text-gray-900">APPEL AUX ARTISTES 📣
+                    <br class="hidden lg:inline-block">Artiste professionnel·le ou amateur·e ?
                 </h1>
-                <p class="mb-8 leading-relaxed">Copper mug try-hard pitchfork pour-over freegan heirloom neutra air plant cold-pressed tacos poke beard tote bag. Heirloom echo park mlkshk tote bag selvage hot chicken authentic tumeric truffaut hexagon try-hard chambray.</p>
+                <p class="mb-8 leading-relaxed">Bonjour à tou·te·s, 🎨 Pourquoi ne pas décorer son intérieur ou offrir une oeuvre originale à ses proches créée et pensée par un artiste local? C’est ce que la Feria d’Art propose ! La Feria d’Art souhaite casser les codes du marché de l’art classique, et ce, en désacralisant l’achat d’une oeuvre d’art tout en offrant une expérience inclusive pour les artistes et le public. Nous vous annonçons avec enthousiasme que la première édition prendra place vendredi 18 et samedi 19 septembre 2020 dans la plus grande occupation temporaire de Belgique, à Bruxelles, au See U.</p>
                 <div class="flex justify-center">
                     <button class="inline-flex text-white bg-indigo-500 border-0 py-2 px-6 focus:outline-none hover:bg-indigo-600 rounded text-lg">Button</button>
                     <button class="ml-4 inline-flex text-gray-700 bg-gray-100 border-0 py-2 px-6 focus:outline-none hover:bg-gray-200 rounded text-lg">Button</button>
