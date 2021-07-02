0bb4b00b044c1f374d634b1102d5fbc9934d34bc
Marcelo Romero-Cors <luisromeroaraya@gmail.com>
2021-07-02 12:25:53 +0200

    Changed Event Controller SortBy

--- resources/views/events/index.blade.php
+++ resources/views/events/index.blade.php
@@ -2,13 +2,10 @@
     <section class="text-gray-600 body-font">
         <div class="container px-5 py-24 mx-auto">
             <div class="flex flex-wrap -m-4">
-
                 @foreach ($events as $event)
                 <x-event-card :event="$event" />
-                @endforeach
-            
+                @endforeach            
             </div>
-        </div>
-      
+        </div>      
     </section>
 </x-guest-layout>
