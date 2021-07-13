0de618627be8ba24f720a750f12e277ea64b6cd2
Marcelo Romero-Cors <luisromeroaraya@gmail.com>
2021-07-13 11:33:56 +0200

    Changed Gallery views

--- resources/views/components/gallery-card.blade.php
+++ resources/views/components/gallery-card.blade.php
@@ -1,18 +1,6 @@
-<div class="p-4 md:w-full">
-    <div class="h-full border-2 border-gray-200 border-opacity-60 rounded-lg overflow-hidden">
-        <img class="lg:h-72 md:h-36 w-full object-cover object-center" src="{{ $event->img_src}}" alt="{{ $event->name }}">
-        <div class="p-6">
-            <h2 class="tracking-widest text-xs title-font font-medium text-gray-400 mb-1">{{ \Carbon\Carbon::parse($event->date_start)->isoFormat('D/M/YYYY HH:mm') }} - {{ \Carbon\Carbon::parse($event->date_end)->isoFormat('D/M/YYYY HH:mm') }}</h2>
-            <h1 class="title-font text-lg font-medium text-gray-900 mb-3">{{ $event->name }}</h1>
-            <!-- <p class="leading-relaxed mb-3">{!! $event->description !!}</p> -->
-            <div class="flex items-center flex-wrap ">
-                <a href="{{ route('gallery.show', $event) }}" class="text-indigo-500 inline-flex items-center md:mb-2 lg:mb-0">{{ __('See More') }}
-                    <svg class="w-4 h-4 ml-2" viewBox="0 0 24 24" stroke="currentColor" stroke-width="2" fill="none" stroke-linecap="round" stroke-linejoin="round">
-                        <path d="M5 12h14"></path>
-                        <path d="M12 5l7 7-7 7"></path>
-                    </svg>
-                </a>
-            </div>
-        </div>
+<div class="flex flex-wrap w-96 bg-gray-100 py-36 px-10 relative m-4">
+    <img alt="gallery" class="w-full object-cover h-full object-center block opacity-25 absolute inset-0" src="{{ $event->img_src}}" alt="{{ $event->name }}">
+    <div class="text-center relative z-10 w-full">
+        <h2 class="text-2xl text-gray-900 font-medium title-font mb-2">{{ $event->name }}</h2>
     </div>
 </div>
