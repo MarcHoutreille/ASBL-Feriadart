4d488a9911898d134e2d2bdb2247caac910879fa
Marcelo Romero-Cors <luisromeroaraya@gmail.com>
2021-07-13 11:50:51 +0200

    Fixed Gallery show link

--- resources/views/components/gallery-card.blade.php
+++ resources/views/components/gallery-card.blade.php
@@ -1,5 +1,7 @@
 <div class="flex flex-wrap w-96 bg-gray-100 py-36 px-10 relative m-4">
-    <img alt="gallery" class="w-full object-cover h-full object-center block opacity-25 absolute inset-0" src="{{ $event->img_src}}" alt="{{ $event->name }}">
+    <a href="{{ route('gallery.show', $event) }}">
+        <img alt="gallery" class="w-full object-cover h-full object-center block opacity-25 absolute inset-0" src="{{ $event->img_src}}" alt="{{ $event->name }}">
+    </a>
     <div class="text-center relative z-10 w-full">
         <h2 class="text-2xl text-gray-900 font-medium title-font mb-2">{{ $event->name }}</h2>
     </div>
