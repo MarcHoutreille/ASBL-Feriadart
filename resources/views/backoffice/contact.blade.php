e41a23904561dea6e94120cc5752768786110a3a
RobbertKlockaerts <robbert.klockaerts@gmail.comm>
2021-07-29 10:10:42 +0200

    remove shadow from bo views

--- resources/views/backoffice/contact.blade.php
+++ resources/views/backoffice/contact.blade.php
@@ -16,7 +16,7 @@
         <!-- CONTACT MESSAGES TABLE -->
         <div class="-my-2 overflow-x-auto sm:-mx-6 lg:-mx-8">
             <div class="py-2 align-middle inline-block min-w-full sm:px-6 lg:px-8">
-                <div class="shadow overflow-hidden">
+                <div class="overflow-hidden">
                     <table class="w-11/12 divide-y divide-gray-200 mx-auto">
                         <thead class="bg-gray-50">
                             <tr>
