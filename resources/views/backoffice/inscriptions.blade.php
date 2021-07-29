eef075026f7b4c1430640a30fa03294ca51e8efe
Marcelo Romero-Cors <luisromeroaraya@gmail.com>
2021-07-29 09:32:34 +0200

    Remove gray border from backoffice

--- resources/views/backoffice/inscriptions.blade.php
+++ resources/views/backoffice/inscriptions.blade.php
@@ -17,7 +17,7 @@
         <!-- INSCRIPTIONS TABLE -->
         <div class="-my-2 overflow-x-auto sm:-mx-6 lg:-mx-8">
             <div class="py-2 align-middle inline-block min-w-full sm:px-6 lg:px-8">
-                <div class="shadow overflow-hidden border-b border-gray-200 sm:rounded-lg">
+                <div class="shadow overflow-hidden">
                     <table class="w-11/12 divide-y divide-gray-200 mx-auto">
                         <thead class="bg-gray-50">
                             <tr>
