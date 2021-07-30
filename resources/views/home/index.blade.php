846a4829cf501b70de1e5aa41ef1b4d5ddf111a4
Marcelo Romero-Cors <luisromeroaraya@gmail.com>
2021-07-30 09:30:54 +0200

    Changed Featured Artist image size

--- resources/views/home/index.blade.php
+++ resources/views/home/index.blade.php
@@ -3,15 +3,15 @@
         <!-- FEATURED ARTIST -->
         @if ($artist)
         <div class="container w-full flex flex-col md:flex-row p-4 mx-auto border-b-2 border-yellow-300">
-            <div class="w-full md:p-4 overflow-hidden">
-                <img alt="content" class="rounded-lg object-cover object-center w-full" src="{{ $artist->img_01 }}">
-            </div>
+            <a href="/artists/artist/{{ $artist->id }}" class="w-full h-96 inline-flex justify-center items-center md:m-4 rounded-lg overflow-hidden">
+                <img alt="content" class="w-full align-center mx-auto object-cover object-center rounded-lg" src="{{ $artist->img_01 }}">
+            </a>
             <div class="w-full flex flex-col items-center justify-center text-center md:text-left md:p-8">
                 <h2 class="md:text-4xl text-3xl mb-4 text-purple-700 uppercase font-mono">{{ __('Featured Artist') }}</h2>
                 <h3 class="font-medium title-font mb-4 text-gray-900 text-lg italic">- {{ $artist->fname }} {{ $artist->lname }} -</h3>
                 <div class="leading-relaxed text-lg mb-8">{!! $artist->bio !!}</div>
-                <div class="flex justify-center mx-auto mb-4">
-                    <a href="/artists/artist/{{ $artist->id }}" class="btn text-lg text-center text-purple-700 bg-yellow-300 hover:bg-yellow-200 border-2 border-purple-700 focus:outline-none rounded py-2 px-6 mb-4">{{ __('View more') }}</a>
+                <div class="flex justify-center mx-auto">
+                    <a href="/artists/artist/{{ $artist->id }}" class="btn text-lg text-center text-purple-700 bg-yellow-300 hover:bg-yellow-200 border-2 border-purple-700 focus:outline-none rounded py-2 px-6">{{ __('View more') }}</a>
                 </div>
             </div>
         </div>
@@ -23,13 +23,13 @@
                 <h2 class="md:text-4xl text-3xl mb-4 text-purple-700 uppercase font-mono">{{ __('Call to artists') }}</h2>
                 <h3 class="font-medium title-font mb-4 text-purple-700 text-lg">{{ __('Professional or amateur artist') }} ?</h3>
                 <div class="leading-relaxed text-lg mb-8">{!! $event->inscription_txt !!}</div>
-                <div class="flex justify-center mx-auto mb-4">
-                    <a href="/events/{{ $event->slug }}" class="btn text-lg text-center text-purple-700 bg-yellow-300 hover:bg-yellow-200 border-2 border-purple-700 focus:outline-none rounded py-2 px-6 mb-4">{{ __('Learn more') }}</a>
+                <div class="flex justify-center mx-auto mb-8 md:mb-0">
+                    <a href="/events/{{ $event->slug }}" class="btn text-lg text-center text-purple-700 bg-yellow-300 hover:bg-yellow-200 border-2 border-purple-700 focus:outline-none rounded py-2 px-6">{{ __('Learn more') }}</a>
                 </div>
             </div>
-            <div class="w-full md:p-4 overflow-hidden">
-                <img alt="content" class="rounded-lg object-cover object-center w-full" src="{{ $event->inscription_img }}">
-            </div>
+            <a href="/events/{{ $event->slug }}" class="w-full md:m-4 overflow-hidden">
+                <img alt="content" class="w-full object-cover object-center rounded-lg" src="{{ $event->inscription_img }}">
+            </a>
         </div>
         @endif
         <!-- NEXT EVENTS -->
